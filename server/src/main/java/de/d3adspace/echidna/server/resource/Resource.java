74bebd044e16fe5fd114e1c4a0dfa4e0da19be8d
Felix Klauke <info@felix-klauke.de>
2019-07-28 22:00:10 +0200

    Begin designing the resource interfaces.

--- server/src/main/java/de/d3adspace/echidna/server/resource/Resource.java
+++ server/src/main/java/de/d3adspace/echidna/server/resource/Resource.java
@@ -2,5 +2,10 @@ package de.d3adspace.echidna.server.resource;
 
 public interface Resource {
 
-
+  /**
+   * Get the underlying resource handle instance.
+   *
+   * @return The instance.
+   */
+  Object getHandle();
 }
--- server/src/main/java/de/d3adspace/echidna/server/resource/ResourceManager.java
+++ server/src/main/java/de/d3adspace/echidna/server/resource/ResourceManager.java
@@ -2,5 +2,17 @@ package de.d3adspace.echidna.server.resource;
 
 public interface ResourceManager {
 
+  /**
+   * Register a resource by the given class.
+   *
+   * @param resourceClazz The resource class.
+   */
+  void registerResource(Class<?> resourceClazz);
 
+  /**
+   * Register a resource by its instance.
+   *
+   * @param resource The instance.
+   */
+  void registerResource(Object resource);
 }
